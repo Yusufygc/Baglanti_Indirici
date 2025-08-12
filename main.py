1418ed9949837b155c1d37d1ac0b8710599d35ce
Yusufygc <yusufygc118@gmail.com>
2025-08-12 12:20:43 +0300

    Artik PyQt5 ile baslatiyoruz

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from arayuz import Arayuz
+# main.py
+import sys
+from PyQt5.QtWidgets import QApplication
+from arayuz import UygulamaArayuzu
 
-if __name__ == "__main__":
-    app = Arayuz()
-    app.mainloop()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    pencere = UygulamaArayuzu()
+    pencere.show()
+    sys.exit(app.exec_())
